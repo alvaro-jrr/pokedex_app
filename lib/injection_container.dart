96931d74c421087e4d94df5cdc299c60844c257c
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-04-14 23:15:08 -0400

    inject dependencies

---
+++ lib/injection_container.dart
@@ -0,0 +1,67 @@
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+import 'package:pokedex_app/core/databases/pokemon_database.dart';
+import 'package:pokedex_app/core/network/network_info.dart';
+import 'package:pokedex_app/core/utils/input_converter.dart';
+import 'package:pokedex_app/features/pokemon/data/data_sources/pokemon_local_data_source.dart';
+import 'package:pokedex_app/features/pokemon/data/data_sources/pokemon_remote_data_source.dart';
+import 'package:pokedex_app/features/pokemon/data/repositories/pokemon_repository_impl.dart';
+import 'package:pokedex_app/features/pokemon/domain/repositories/pokemon_repository.dart';
+import 'package:pokedex_app/features/pokemon/domain/use_cases/add_favorite_pokemon.dart';
+import 'package:pokedex_app/features/pokemon/domain/use_cases/get_concrete_pokemon.dart';
+import 'package:pokedex_app/features/pokemon/domain/use_cases/get_favorite_pokemon.dart';
+import 'package:pokedex_app/features/pokemon/domain/use_cases/remove_favorite_pokemon.dart';
+import 'package:pokedex_app/features/pokemon/presentation/bloc/bloc.dart';
+
+final sl = GetIt.instance;
+
+void init() {
+  //* Features - Pokemon.
+  // Bloc.
+  sl.registerFactory(
+    () => PokemonBloc(
+      addFavoritePokemon: sl(),
+      getFavoritePokemons: sl(),
+      removeFavoritePokemon: sl(),
+      getConcretePokemon: sl(),
+      inputConverter: sl(),
+    ),
+  );
+
+  // Use Cases.
+  sl.registerLazySingleton(() => AddFavoritePokemon(sl()));
+  sl.registerLazySingleton(() => GetFavoritePokemon(sl()));
+  sl.registerLazySingleton(() => RemoveFavoritePokemon(sl()));
+  sl.registerLazySingleton(() => GetConcretePokemon(sl()));
+
+  // Repository.
+  sl.registerLazySingleton<PokemonRepository>(
+    () => PokemonRepositoryImpl(
+      localDataSource: sl(),
+      remoteDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  // Data Sources.
+  sl.registerLazySingleton<PokemonLocalDataSource>(
+    () => PokemonLocalDataSourceImpl(database: sl()),
+  );
+
+  sl.registerLazySingleton<PokemonRemoteDataSource>(
+    () => PokemonRemoteDataSourceImpl(client: sl()),
+  );
+
+  //* Core.
+  final database = PokemonDatabase.db;
+
+  sl.registerLazySingleton(() => database);
+  sl.registerLazySingleton(() => InputConverter());
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  //* External.
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
